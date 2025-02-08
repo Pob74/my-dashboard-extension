8eb3f2d3ec5d45a6903d59a8088765e3acd302b9
zeljko haberstok <zeljkohaberstok@gmail.com>
2025-02-08 15:47:26 +0100

    added change time to pomodoro timer

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -8,6 +8,8 @@ const PomodoroTimer = () => {
   const [currentTime, setCurrentTime] = useState(new Date())
   const { setTimerPomodore } = useSettings()
 
+  const [selectedTime, setSelectedTime] = useState(25)
+
   useEffect(() => {
     const clockTimer = setInterval(() => {
       setCurrentTime(new Date())
@@ -32,7 +34,13 @@ const PomodoroTimer = () => {
   const toggleTimer = () => setIsActive(!isActive)
   const resetTimer = () => {
     setIsActive(false)
-    setTimeLeft(25 * 60)
+    setTimeLeft(selectedTime * 60)
+  }
+
+  const handleChange = (event) => {
+    const newTime = parseInt(event.target.value, 10) // Convert to number
+    setSelectedTime(newTime)
+    setTimeLeft(newTime * 60) // Update timeLeft when new time is selected
   }
 
   const hours = currentTime.getHours()
@@ -45,7 +53,8 @@ const PomodoroTimer = () => {
 
   const radius = 140 // Radius of the circle
   const circumference = 2 * Math.PI * radius
-  const progress = ((25 * 60 + timeLeft) / (25 * 60)) * circumference // Calculate progress
+  const progress =
+    ((selectedTime * 60 + timeLeft) / (selectedTime * 60)) * circumference // Calculate progress
 
   const timerMinutes = Math.floor(timeLeft / 60)
   const timerSeconds = timeLeft % 60
@@ -53,8 +62,6 @@ const PomodoroTimer = () => {
   return (
     <div className="absolute inset-0 z-50 flex flex-col items-center justify-center h-screen bg-gray-900/75 text-gray-500 ">
       <div className="text-4xl font-bold mb-8 tracking-wider">On Break</div>
-
-      {}
 
       <div className="relative flex w-80 h-80">
         <svg className="absolute w-full h-full">
@@ -73,7 +80,6 @@ const PomodoroTimer = () => {
         </svg>
 
         {/* Clock Hands */}
-
         <svg className="absolute w-full h-full">
           <line
             x1="150"
@@ -86,9 +92,6 @@ const PomodoroTimer = () => {
             transform={`rotate(${hourHandAngle} 150 150)`}
             style={{ transition: "transform 0.1s linear" }}
           />
-
-          {/* Minute Hand */}
-
           <line
             x1="150"
             y1="150"
@@ -100,9 +103,6 @@ const PomodoroTimer = () => {
             transform={`rotate(${minuteHandAngle} 150 150)`}
             style={{ transition: "transform 0.1s linear" }}
           />
-
-          {/* Second Hand */}
-
           <line
             x1="150"
             y1="150"
@@ -115,6 +115,7 @@ const PomodoroTimer = () => {
             style={{ transition: "transform 0.1s linear" }}
           />
         </svg>
+
         <div className="absolute top-[56%] left-[47%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center text-6xl font-mono">
           {timerMinutes < 10 ? `0${timerMinutes}` : timerMinutes}:
           {timerSeconds < 10 ? `0${timerSeconds}` : timerSeconds}
@@ -144,6 +145,30 @@ const PomodoroTimer = () => {
         alt="exit"
         className="w-12 h-12 cursor-pointer mt-6 transition duration-300 ease-in-out hover:opacity-80"
       />
+
+      {/* Time selector dropdown */}
+      <div className="mt-6">
+        <select
+          name="selectedTime"
+          id="selectedTime"
+          onChange={handleChange}
+          value={selectedTime}
+          className="px-4 py-2 rounded-md text-lg bg-black"
+        >
+          <option value="5">5 minutes</option>
+          <option value="10">10 minutes</option>
+          <option value="15">15 minutes</option>
+          <option value="20">20 minutes</option>
+          <option value="25">25 minutes</option>
+          <option value="30">30 minutes</option>
+          <option value="35">35 minutes</option>
+          <option value="40">40 minutes</option>
+          <option value="45">45 minutes</option>
+          <option value="50">50 minutes</option>
+          <option value="55">55 minutes</option>
+          <option value="60">1 hour</option>
+        </select>
+      </div>
     </div>
   )
 }
